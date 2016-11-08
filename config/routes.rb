9903bce02f0b1cb1077bd177bb90f03c4b56098d
Alexander Harris <abharris@pugetsound.edu>
2016-11-08 12:06:22 -0800

    add resources :articles,  routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
+
+  # get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles
 
   # tell rails to map requests to http://localhost:3000/welcome/index
   root 'welcome#index'
