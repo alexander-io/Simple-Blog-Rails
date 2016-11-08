4353db83d6ea6e8db45e9d1387cecc1ad06a1da3
Alexander Harris <abharris@pugetsound.edu>
2016-11-08 13:45:04 -0800

    update controller to save article attribute to database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    # every rails model can be initialized with respective attributes
+    # these attributes are automatically mapped to respective database locations
+    # do that :
+    @article = Article.new(params[:article])
+
+    # @article.save is responsible for saving the model in the database.
+    @article.save
+    redirect_to @article
+    # render plain: params[:article].inspect
   end
 
 end
