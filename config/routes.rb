af66f0bb0e3e742edbe09f8f518122b0b9be5043
Alexander Harris <abharris@pugetsound.edu>
2016-11-08 12:00:23 -0800

    update view & route to welcome/index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # tell rails to map requests to http://localhost:3000/welcome/index
+  root 'welcome#index'
 end
