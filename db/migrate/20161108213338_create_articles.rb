34593b40541fdb08d502c9f23103ad3f7a365e2a
Alexander Harris <abharris@pugetsound.edu>
2016-11-08 13:40:20 -0800

    add CreateArticles migration $ rails db:migrate to generate schema

--- db/migrate/20161108213338_create_articles.rb
+++ db/migrate/20161108213338_create_articles.rb
@@ -1,4 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
+  # this file is created by issuing :
+  #   $ rails generate model
+  # this has created this migration file inside db migrate directory.
+  # migrations are ruby classes that are designed to make it simple to created
+  # and modify database tables. rails uses rake commands to run migrations, and its
+  # possible to undo a moigration after its been appliied to the database.
+  # migration filenames include a timestamp to ensure that theyre processed in the
+  # order that they were created
   def change
     create_table :articles do |t|
       t.string :title
