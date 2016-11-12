4849279852f07ce02d691c9609762ea11cc76ae7
Alexander Harris <abharris@pugetsound.edu>
2016-11-11 19:24:05 -0800

    edit app/models/article.rb so that each article has_many :comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # ensure that artiicle titles are > 5 characters in length
   validates :title, presence: true, length: { minimum: 5 }
 end
